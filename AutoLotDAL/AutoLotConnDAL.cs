d6119e80bf7b10fc2c87b2fac78469fafe5b0fe2
Paweł Jasiaczyk <pawel@DebianOptiplex755>
2015-07-22 23:22:07 +0200

    First commit

---
+++ AutoLotDAL/AutoLotConnDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// I need to try write it as a ProviderFactory model
+// Current version is targetet for MySQL database
+using System.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace AutoLotConnectionLayer
+{
+	public class InventoryDAL
+	{
+	}
+}
+
